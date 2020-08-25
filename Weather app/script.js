0149250f3c5a164413fed862ec29e6b471bf6adc
Keshav Gupta <66676132+keshavgupta62@users.noreply.github.com>
2020-08-25 11:31:38 +0530

    I am adding the debounce function.

--- Weather app/script.js
+++ Weather app/script.js
@@ -1,4 +1,3 @@
-var button = document.querySelector('.button');
 var inputValue = document.querySelector('.inputValue');
 var cityName = document.querySelector('.name');
 var desc = document.querySelector('.desc');
@@ -7,13 +6,12 @@ var minMax = document.querySelector('.minMax');
 var wind = document.querySelector('.wind');
 var iconId = document.querySelector('.iconId');
 
-button.addEventListener('click', function() {
+function getData() {
     var url = 'https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=056da36d1478645a30abbc1ce79254c0'
     fetch(url).then((response) => {
         return response.json();
     })
     .then((data) => {
-        console.log(data);
         cityName.innerHTML = `${data.name}`+', '+`${data.sys.country}`;
         iconId.innerHTML  = `<img src="icons/${data.weather[0].icon}.png" />`;
         desc.innerHTML = 'Weather : '+`${data.weather[0].description}`;
@@ -33,4 +31,17 @@ button.addEventListener('click', function() {
         minMax.innerHTML = '';
         wind.innerHTML = '';
     })
-})
+}
+
+function doSomeMagic(fn, d) {
+    let timer;
+    return function() {
+        let context = this;
+        clearTimeout(timer);
+        timer = setTimeout(()=> {
+            fn.apply(context, arguments)
+        }, d);
+    }
+}
+
+const betterFunction = doSomeMagic(getData, 300);
